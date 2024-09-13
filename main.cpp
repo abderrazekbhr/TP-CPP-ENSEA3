571a7b54ceae38593772e5c4416c49e0e06e5c47
abderrazekbhr <abderrazek455@gmail.co>
2024-09-13 20:48:55 +0200

    init documentation

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
+// <------- libraries ------>
 #include <iostream>
+// <------- classes ------>
 #include "Pokedex.h"
 #include "Pokeball.h"
 #include "SetOfPokemon.h"
@@ -12,10 +14,6 @@
 #include "./state_pattern/h_files/SafeExploration.h"
 #include "./state_pattern/h_files/Exploration.h"
 #include "./state_pattern/h_files/Combat.h"
-/*
-g++ ./main.cpp ./Pokedex.cpp ./Pokeball.cpp ./SetOfPokemon.cpp ./Pokemone.cpp ./Game.cpp ./Player.cpp ./PokemonParty.cpp ./state_pattern/cpp_files/Attente.cpp ./state_pattern/cpp_files/Init.cpp ./state_pattern/cpp_files/SafeExploration.cpp ./state_pattern/cpp_files/Exploration.cpp ./state_pattern/cpp_files/Combat.cpp  -o newmain && ./newmain
-
-*/
 
 int secondMain()
 {
@@ -48,26 +46,40 @@ int secondMain()
     // pokeball->affiche();
     return 0;
 }
-
+// <------- main function ------>
 int main()
 {
+    // launch the combat variable
     std::string doYouWantToCombat;
+    // define the players id
+    int playerId1 = 0;
+    int playerId2 = 1;
+
+    // Attente state is created when the game is created
+    // handel function is called by calling the request function
+    // the setState function is called to change the state
     Game gamePlay = Game();
     gamePlay.request();
+    //init state is created
     gamePlay.setState(new Init());
     gamePlay.request();
     do
     {
+        // SafeExploration state is created
         gamePlay.setState(new SafeExploration());
         gamePlay.request();
+        // Exploration state is created
         gamePlay.setState(new Exploration());
         gamePlay.request();
+        // check if the player wants to combat
         std::cin >> doYouWantToCombat;
     } while (doYouWantToCombat == "No");
-    int playerId1 = 0;
-    int playerId2 = 1;
-
-    gamePlay.setState(new Combat(new PokemonParty(playerId1, playerId2, {0, 1, 2, 3, 4, 5}, {3, 4, 5, 6, 7, 8})));
+    
+    // Combat state is created
+    gamePlay.setState(new Combat(new PokemonParty(playerId1, playerId2, {0, 1, 2, 3, 4, 5}, {0, 1, 2, 3, 4, 5})));
     gamePlay.request();
     return 0;
 }
+/*
+g++ ./main.cpp ./Pokedex.cpp ./Pokeball.cpp ./SetOfPokemon.cpp ./Pokemone.cpp ./Game.cpp ./Player.cpp ./PokemonParty.cpp ./state_pattern/cpp_files/Attente.cpp ./state_pattern/cpp_files/Init.cpp ./state_pattern/cpp_files/SafeExploration.cpp ./state_pattern/cpp_files/Exploration.cpp ./state_pattern/cpp_files/Combat.cpp  -o newmain && ./newmain
+*/
